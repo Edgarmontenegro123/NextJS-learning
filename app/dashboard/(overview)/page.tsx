617f0c4f50a3c685ab91872d2383114ada437bbf
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-06-13 22:52:50 -0300

    🐛 Fix the bug with cardsSkeleton loading (all at the same time)

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
 import {Suspense} from 'react'
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardSkeleton } from "@/app/ui/skeletons";
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
 import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
@@ -12,7 +12,7 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                 <Suspense fallback={<CardSkeleton/>}>
+                 <Suspense fallback={<CardsSkeleton/>}>
                      <CardWrapper />
                  </Suspense>
             </div>
