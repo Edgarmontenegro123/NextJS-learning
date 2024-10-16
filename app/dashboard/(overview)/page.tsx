61794d3e42edc6163b00f752b17787db5148b431
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-10-16 03:43:19 -0300

    ⚰️ Delete dead code

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,9 @@
+import {Suspense} from 'react';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import { lusitana } from '@/app/ui/fonts';
-import {Suspense} from 'react'
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
-import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
     return (
@@ -11,12 +11,12 @@ export default async function Page() {
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
                  <Suspense fallback={<CardsSkeleton/>}>
                      <CardWrapper />
                  </Suspense>
             </div>
-            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
                 <Suspense fallback={<RevenueChartSkeleton/>}>
                     <RevenueChart />
                 </Suspense>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,9 @@
 'use server'
 
-import { z } from 'zod'
-import { sql } from '@vercel/postgres'
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
